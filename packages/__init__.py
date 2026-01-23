54788aa37e80e9c90abff3455585b7cb65c89590
SabanaSultana <sabanasultana132@gamil.com>
2026-01-23 11:15:05 +0530

    python packages

---
+++ packages/__init__.py
@@ -0,0 +1,3 @@
+'''' This is the __init__.py file for the packages module. And to access packages this file is must present in the folder. '''
+
+''' For Custom Package Creation'''
---
+++ test_custom_package.py
@@ -0,0 +1,6 @@
+from packages.maths import addition
+
+print("The sum of 5 and 3 is:", addition(5, 3))
+
+
+''' This file and the packages folder should be in the same directory for the import to work correctly. '''
